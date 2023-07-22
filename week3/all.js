33323000d0a70403210a678abba7402851d34698
zoechen <creepylight1030@gmail.com>
2023-07-22 17:58:30 +0800

    upload Week3 Task2 + update Task4

--- week3/all.js
+++ week3/all.js
@@ -15,13 +15,11 @@ let renderCount = 0; // 計算總共渲染幾筆資料
 
 fetch(src).then(function(response){
 	return response.json();
-})
-  .then(function(data){
+}).then(function(data){
     allData = data.result.results // JSON格式資料
     renderData(allData); // 連結成功就直接執行渲染 
     // console.log(allData)
-})
-  .catch(function(error){
+}).catch(function(error){
     console.log("發生錯誤" + error);
 });
 
@@ -42,50 +40,50 @@ function renderData(data) {
   
   data.forEach(function(attraction,index) {
   // 創建卡片 div
-  const cardDiv = document.createElement("div"); 
+    const cardDiv = document.createElement("div"); 
   
   // 判斷若未渲染過且為前三筆資料，套用 "card-promo" 樣式，其餘皆套用 "card-title" 樣式
-  if (renderCount === 0 && index < 3) {
-    cardDiv.classList.add("card-promo");
-  } else {
-    // 超過 12 筆 "card-title" 就結束函式執行
-    if (titleCount >= 12) {
-      return; 
+    if (renderCount === 0 && index < 3) {
+      cardDiv.classList.add("card-promo");
+    } else {
+      // 超過 12 筆 "card-title" 就結束函式執行
+      if (titleCount >= 12) {
+        return; 
+      }
+      cardDiv.classList.add("card-title");
+      titleCount++;
+      // console.log("有印製");
     }
-    cardDiv.classList.add("card-title");
-    titleCount++;
-    // console.log("有印製");
-  }
 
-  // 創建 <img> 並設定 src 和 alt 屬性
-  const img = document.createElement("img");
-  img.src = "https" + attraction.file.split("https")[1]; // 以 https 為節點做分割，取第一筆，並把文字 https 加回去
-  img.alt = "";
+    // 創建 <img> 並設定 src 和 alt 屬性
+    const img = document.createElement("img");
+    img.src = "https" + attraction.file.split("https")[1]; // 以 https 為節點做分割，取第一筆，並把文字 https 加回去
+    img.alt = "";
 
-  // 創建包照片的 <div>
-  const imgDiv = document.createElement("div");
-  imgDiv.classList.add("imgDiv");
+    // 創建包照片的 <div>
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("imgDiv");
 
-  // 創建包含標題的 <div>
-  const contentDiv = document.createElement("div");
-  contentDiv.classList.add("content");
-  
-  // 創建 <h3> 並設定標題文字
-  const h3 = document.createElement("h3");
-  h3.textContent = attraction.stitle;
+    // 創建包含標題的 <div>
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("content");
+    
+    // 創建 <h3> 並設定標題文字
+    const h3 = document.createElement("h3");
+    h3.textContent = attraction.stitle;
 
-  // 將 img 成為 imgDiv 的子層
-  imgDiv.appendChild(img);
-  
-  // 將 h3 成為 contentDiv 的子層
-  contentDiv.appendChild(h3);
+    // 將 img 成為 imgDiv 的子層
+    imgDiv.appendChild(img);
+    
+    // 將 h3 成為 contentDiv 的子層
+    contentDiv.appendChild(h3);
 
-  // 將 imgDiv 及 contentDiv 成為 cardDiv 的子層
-  cardDiv.appendChild(imgDiv);
-  cardDiv.appendChild(contentDiv);
-  
-  // 將 cardDiv 成為 container 的子層
-  cards.appendChild(cardDiv);
+    // 將 imgDiv 及 contentDiv 成為 cardDiv 的子層
+    cardDiv.appendChild(imgDiv);
+    cardDiv.appendChild(contentDiv);
+    
+    // 將 cardDiv 成為 container 的子層
+    cards.appendChild(cardDiv);
   });
 
   // 若未渲染過，renderCount+15，排除掉首次渲染的 15 筆資料，讓後續維持增加 12 筆
--- week3/all.py
+++ week3/all.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# === Task1 ===
 import urllib.request as request
 import json
 import csv
@@ -65,3 +66,60 @@ if api_data:
   mrt_csv_file = "mrt.csv"
   save_to_attraction_csv(api_data, attraction_csv_file)
   save_to_mrt_csv(api_data, mrt_csv_file)
+
+
+
+# === Task2 ===
+import urllib.request as req
+
+def get_post_time(perPageURL):
+  # 建立一個 Request 物件，附加 Request Headers 的資訊讓自己看起來像一般使用者
+  request = req.Request(perPageURL, headers = {
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+  })
+  with req.urlopen(request) as response:
+    data =  response.read().decode("utf-8")
+  # 使用 BeautifulSoup 解析原始碼，取得每篇文章的標題
+  import bs4
+  root = bs4.BeautifulSoup(data, "html.parser")
+  postTimeSet = root.find_all("span", class_="article-meta-value") # 尋找所有 class_="article-meta-value" 的 span 標籤
+  for postTime in postTimeSet:
+    eachPostTime = postTime.text # 提取發文時間
+  return eachPostTime
+
+def get_data(url):
+  # 建立一個 Request 物件，附加 Request Headers 的資訊讓自己看起來像一般使用者
+  request = req.Request(url, headers = {
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+  })
+  with req.urlopen(request) as response:
+    data =  response.read().decode("utf-8")
+
+  # 使用 BeautifulSoup 解析原始碼，取得每篇文章的標題
+  import bs4
+  root = bs4.BeautifulSoup(data, "html.parser")
+  # find 找單一；find_all 找所有符合的項目，並以 List 方式列出來
+  titles = root.find_all("div", class_="title") # 尋找所有 class="title" 的 div 標籤
+  pushNum = root.find_all("div", class_="nrec")
+
+  # 創建 movie.txt 檔案， a 是附加的意思，若檔案已經存在，資料會被附加在檔案內容的末尾，而不是覆寫原有的內容
+  with open("movie.txt", "a", encoding="utf-8") as file:
+    for title, nrec in zip(titles, pushNum): # 遍歷 titles、pushNum 去跑
+      if title.a != None: # 剔除沒有 a 標籤（被刪除）的內容
+        eachTitle = title.a.string # 取出其中的標題文字
+        perPageURL = "https://www.ptt.cc" + title.a["href"] # 取出該標題的網址
+      span = nrec.find("span")
+      eachPushNum = span.text if span else "0" # 取出 span 中的文字，無則顯示 0
+      eachPostTime = get_post_time(perPageURL) # 用該標題網址呼叫 get_post_time 函式進去提取發文時間
+    
+      # 將資料寫入檔案
+      file.write(f"{eachTitle},{eachPushNum},{eachPostTime}\n")
+
+  nextLink = root.find("a", string = "‹ 上頁") # 找到內文是 "‹ 上頁" 的 a 標籤，並抓取
+  return nextLink["href"] # 資料長相：/bbs/movie/index9624.html
+
+pageURL = "https://www.ptt.cc/bbs/movie/index.html" # 電影版首頁
+count = 0
+while count < 3: # 找三頁
+  pageURL = "https://www.ptt.cc" + get_data(pageURL)
+  count+=1
